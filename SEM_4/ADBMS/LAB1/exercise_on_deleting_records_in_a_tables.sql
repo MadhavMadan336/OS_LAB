5aed709c12dbcb322c2186cd2a98f178113765ee
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-02-14 20:28:45 +0530

    Create exercise_on_deleting_records_in_a_tables.sql

---
+++ SEM_4/ADBMS/LAB1/exercise_on_deleting_records_in_a_tables.sql
@@ -0,0 +1,9 @@
+a.	Delete all salesman from the Salesman_Master whose salaries are equal to Rs.3500.
+b.	Delete all products from Product_Master where the quantity on hand is equal to 100.
+c.	Delete from Client_Master where the column state holds the value ‘Tamil Nadu’.
+
+a)DELETE FROM SALESMAN_MASTER WHERE SALARY = 3500;
+B)DELETE FROM PRODUCT_MASTER
+WHERE QTYONHAND = 100;
+C)DELETE FROM CLIENT_MASTER
+WHERE STATE = 'Tamil Nadu';
