90fc7bf5357d0854035239e47e8133d53951c64d
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-02-15 13:54:51 +0530

    Create exercise_on_altering_the_table_structure.sql

---
+++ SEM_4/ADBMS/LAB1/exercise_on_altering_the_table_structure.sql
@@ -0,0 +1,6 @@
+a.	Add a column called ‘Telephone’ of data type integer to the Client_Master table.
+b.	Change the size off SellPrice column in Product _Master to 10, 2.
+
+
+a)	ALTER TABLE CLIENT_MASTER ADD TELEPHONE INTEGER;
+b)	ALTER TABLE PRODUCT_MASTER MODIFY SELLPRICE DECIMAL(10,2);
