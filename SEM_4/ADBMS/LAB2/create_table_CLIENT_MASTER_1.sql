fe9728b4d259feeb6092530dbf2852618fee96ca
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-02-21 10:25:30 +0530

    Update create_table_CLIENT_MASTER_1.sql

--- SEM_4/ADBMS/LAB2/create_table_CLIENT_MASTER_1.sql
+++ SEM_4/ADBMS/LAB2/create_table_CLIENT_MASTER_1.sql
@@ -1,3 +1,23 @@
 create table CLIENT_MASTER_1
 (clientno varchar(6) check (clientno like 'C%') primary key, Name varchar(20) not null,
 address_1 varchar(30), address_2 varchar(30), city varchar(15), pincode number(8), state varchar(15), baldue number(10,2))
+
+
+
+
+
+#value insert
+INSERT ALL
+ 
+INTO CLIENT_MASTER_1 VALUES ('C00001', 'Ivan bayross', '', '',
+'Mumbai', 400054, 'Maharashtra', 15000)
+INTO CLIENT_MASTER_1 VALUES ('C00002', 'Mamta muzumdar', '', '',
+'Madras', 780001, 'Tamil nadu', 0)
+INTO CLIENT_MASTER_1 VALUES ('C00003', 'Chhaya bankar', '', '',
+'Mumbai', 400057, 'Maharashtra', 5000)
+INTO CLIENT_MASTER_1 VALUES ('C00004', 'Ashwini joshi', '', '',
+'Bangalore', 560001, 'Karnataka', 0)
+INTO CLIENT_MASTER_1 VALUES ('C00005', 'Hansel colaco', '', '',
+'Mumbai', 400060, 'Maharashtra', 2000)
+INTO CLIENT_MASTER_1 VALUES ('C00006', 'Deepak sharma', '', '',
+'Mangalore', 560050, 'Karnataka', 0) SELECT * FROM dual;
