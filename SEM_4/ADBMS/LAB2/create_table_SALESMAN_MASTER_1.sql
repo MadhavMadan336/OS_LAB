3a7ceb22305319eae751c5d96fbd5d236b4c6913
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-02-23 11:15:14 +0530

    Update create_table_SALESMAN_MASTER_1.sql

--- SEM_4/ADBMS/LAB2/create_table_SALESMAN_MASTER_1.sql
+++ SEM_4/ADBMS/LAB2/create_table_SALESMAN_MASTER_1.sql
@@ -4,3 +4,17 @@ city varchar(20), pincode number(8), state varchar(20),
 salamt number(8,2) check(salamt!=0) not null , tgttoget number(6,2) check(tgttoget!=0) not null, ytdsales number(6,2) not null,
 remarks varchar(60)
 );
+
+
+
+
+INSERT ALL
+INTO SALESMAN_MASTER_1 VALUES('S00001', 'Aman', 'A/14', 'Worli',
+'Mumbai', 400002, 'Maharashtra', NULL, NULL, NULL,'')
+INTO SALESMAN_MASTER_1 VALUES('S00002', 'Omkar', '65',
+'Nariman', 'Mumbai', 400001, 'Maharashtra', NULL, NULL, NULL,'')
+ 
+INTO SALESMAN_MASTER_1 VALUES('S00003', 'Raj', 'P-7', 'Bandra',
+'Mumbai', 400032, 'Maharashtra', NULL, NULL, NULL,'')
+INTO SALESMAN_MASTER_1 VALUES('S00004', 'Ashish', 'A/5', 'Juhu',
+'Mumbai', 400044, 'Maharashtra', NULL, NULL, NULL,'') SELECT * FROM dual;
