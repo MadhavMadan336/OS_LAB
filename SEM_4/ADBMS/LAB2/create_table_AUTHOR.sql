e69884de5dc09b8fe1becf0d79e9a74b7e9f6442
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-02-24 21:23:40 +0530

    Create create_table_AUTHOR.sql

---
+++ SEM_4/ADBMS/LAB2/create_table_AUTHOR.sql
@@ -0,0 +1,5 @@
+Create table AUTHOR(
+Author_ID varchar(5) primary key, Lastname varchar(15) not null, Firstname varchar(15) not null, Email varchar(50),
+City varchar(15), Country varchar(15)
+ 
+);
