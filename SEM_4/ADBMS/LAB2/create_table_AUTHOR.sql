8382f39568c51098b7ce9e86d730d6d9a7a2e4ee
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-02-27 17:09:15 +0530

    Update create_table_AUTHOR.sql

--- SEM_4/ADBMS/LAB2/create_table_AUTHOR.sql
+++ SEM_4/ADBMS/LAB2/create_table_AUTHOR.sql
@@ -3,3 +3,15 @@ Author_ID varchar(5) primary key, Lastname varchar(15) not null, Firstname varch
 City varchar(15), Country varchar(15)
  
 );
+
+
+INSERT ALL
+INTO AUTHOR (Author_ID, Lastname, Firstname, Email, City, Country) VALUES ('A0001', 'Smith', 'John', 'jsmith@example.com', 'New York',
+'USA')
+INTO AUTHOR (Author_ID, Lastname, Firstname, Email, City, Country) VALUES ('A0002', 'Johnson', 'Emma', 'emma.j@example.com', 'London',
+'UK')
+INTO AUTHOR (Author_ID, Lastname, Firstname, Email, City, Country) VALUES ('A0003', 'Garcia', 'Maria', 'maria.g@example.com', 'Madrid',
+'Spain')
+INTO AUTHOR (Author_ID, Lastname, Firstname, Email, City, Country) VALUES ('A0004', 'Kim', 'Sung', 'sung.kim@example.com', 'Seoul', 'South
+Korea')
+SELECT * FROM dual;
