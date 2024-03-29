e803ec81f68be17bfabb3162176c991fe1f642d9
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-03-29 12:24:42 +0530

    Update Supplier.sql

--- SEM_4/ADBMS/LAB3/Supplier.sql
+++ SEM_4/ADBMS/LAB3/Supplier.sql
@@ -5,3 +5,12 @@ CREATE TABLE Supplier (
  scity VARCHAR(255),
  turnover DECIMAL(10, 2)
 );
+
+
+
+INSERT ALL
+ INTO Supplier (scode, sname, scity, turnover) VALUES(1, 'ram', 'dehradun', 
+10000)
+ INTO Supplier (scode, sname, scity, turnover) VALUES(2, 'mohan', 'delhi', 15000)
+ INTO Supplier (scode, sname, scity, turnover) VALUES(3, 'shyam', 'bihar', 12000)
+SELECT * FROM dual;
