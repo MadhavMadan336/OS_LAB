0e255cb2287050339a3f378ad637a4738c59ab8e
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-03-30 12:44:13 +0530

    Update Part.sql

--- SEM_4/ADBMS/LAB3/Part.sql
+++ SEM_4/ADBMS/LAB3/Part.sql
@@ -6,3 +6,13 @@ CREATE TABLE Part (
  cost DECIMAL(10, 2),
  sellingprice DECIMAL(10, 2)
 );
+
+
+ INSERT ALL
+ INTO Part (pcode, weigh, color, cost, sellingprice) VALUES (101, 5.5, 'Red', 
+50.00, 75.00)
+ INTO Part (pcode, weigh, color, cost, sellingprice) VALUES (102, 8.0, 'Blue', 
+80.00, 120.00)
+ INTO Part (pcode, weigh, color, cost, sellingprice) VALUES (103, 3.3, 'Green', 
+30.00, 50.00)
+SELECT * FROM dual;
