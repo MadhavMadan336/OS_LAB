9dd9327354cc622a92929228d6a4e3a3f2014fcb
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-03-31 14:22:48 +0530

    Update Supplier_part.sql

--- SEM_4/ADBMS/LAB3/Supplier_part.sql
+++ SEM_4/ADBMS/LAB3/Supplier_part.sql
@@ -7,3 +7,10 @@ CREATE TABLE Supplier_Part (
  FOREIGN KEY (scode) REFERENCES Supplier(scode),
  FOREIGN KEY (pcode) REFERENCES Part(pcode)
 );
+
+INSERT ALL
+ INTO Supplier_Part (scode, pcode, qty) VALUES (1, 101, 200)
+ INTO Supplier_Part (scode, pcode, qty) VALUES (1, 102, 150)
+ INTO Supplier_Part (scode, pcode, qty) VALUES (2, 102, 100)
+ INTO Supplier_Part (scode, pcode, qty) VALUES (3, 103, 300)
+SELECT * FROM dual;
