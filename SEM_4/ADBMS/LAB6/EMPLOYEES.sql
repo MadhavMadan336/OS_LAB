79fde9f743ea66ad6118da3720f27c9a7e4a1ece
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-04-22 21:13:47 +0530

    Update EMPLOYEES.sql

--- SEM_4/ADBMS/LAB6/EMPLOYEES.sql
+++ SEM_4/ADBMS/LAB6/EMPLOYEES.sql
@@ -6,3 +6,10 @@ DOB DATE,
 Salary NUMERIC(25, 2) NOT NULL,
 Department_id CHAR(10)
 );
+
+
+1)	Create View of name emp_view and the column would be Employee_id, Last_Name, salary and department_id only.
+
+                CREATE VIEW emp_view AS
+       SFLECT Employee_id, Last_Name, Salary, Department_id
+       FROM EMPLOYEES
