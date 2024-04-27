8a6a4458c6654ad267e999eec18287567b947b82
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-04-27 18:09:58 +0530

    Update query.sql

--- SEM_4/ADBMS/LAB7/query.sql
+++ SEM_4/ADBMS/LAB7/query.sql
@@ -17,3 +17,14 @@ CREATE INDEX employee_id_reverse_idx ON EMPLOYEES(employee_id DESC);
 
 4)	Create a unique and composite index on employee_id and check whether there is duplicity of tuples or not.
 CREATE UNIQUE INDEX composite_employee_dept_idx ON EMPLOYEES(employee_id, department_id);
+
+
+5)	Create Function-based indexes defined on the SQL functions UPPER(column_name) or LOWER(column_name) to facilitate case-insensitive searches(on column Last_Name).
+CREATE INDEX last_name_upper_idx ON EMPLOYEES(UPPER(Last_Name));
+CREATE INDEX last_name_lower_idx ON EMPLOYEES(LOWER(Last_Name));
+
+
+6)	Drop the function based index on column Last_Name.
+
+DROP INDEX last_name_upper_idx;
+DROP INDEX last_name_lower_idx;
